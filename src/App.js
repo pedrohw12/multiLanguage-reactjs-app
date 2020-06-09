dac92709aececdd2c08afdfc3946da947d3f57de
Pedro Wanderley <peu-zico>
2020-06-09 11:17:09 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useTranslation } from 'react-i18next';
+import i18next from 'i18next';
 
 function App() {
+  const { t } = useTranslation();
+
+  function handleClick(lang) {
+    i18next.changeLanguage(lang)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <nav style={{ width: '100%', padding: '2rem 0', backgroundColor:'gray' }}>
+        <button onClick={() => handleClick('ru')}>ru</button>
+      <button onClick={() => handleClick('en')}>en</button>
+      <h1>{t('Welcome to React')}</h1>
+        </nav>
+        <header className="App-header"> 
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            <h3> {t('Welcome.1')} </h3> <h3> {t('To.1')} </h3>
+          </p>
+        </header>
     </div>
   );
 }
